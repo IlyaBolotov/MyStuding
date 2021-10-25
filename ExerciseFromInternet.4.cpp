3c88d37e0809064f90ed5889468873527f1b1bde
Ilya Bolotov <92567941+IlyaBolotov@users.noreply.github.com>
2021-10-26 00:08:14 +0200

    Add files via upload

---
+++ ExerciseFromInternet.4.cpp
@@ -0,0 +1,24 @@
+﻿/*
+https://acmp.ru/index.asp?main=task&id_task=16
+*/
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	int N;     //Количество кубиков
+	int p = 1;	 //Минимальное число кубиков на уровень
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите количество кубиков" << endl;
+	cin >> N;
+	while (N >= p)
+	{
+		N = N - p;
+		if (N >= p)
+		{
+			p++;
+		}
+	}
+	cout << "Число лесенок: " << p << endl;
+		return 0;
+}
