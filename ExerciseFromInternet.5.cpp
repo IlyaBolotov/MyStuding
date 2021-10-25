e38dc392e76b9ce3cd9a7cdb8cb197d6904f9cd4
Ilya Bolotov <92567941+IlyaBolotov@users.noreply.github.com>
2021-10-26 00:22:28 +0200

    Add files via upload

---
+++ ExerciseFromInternet.5.cpp
@@ -0,0 +1,29 @@
+﻿/*
+https://acmp.ru/index.asp?main=task&id_task=18
+*/
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int n; //Число, факториал которого находим
+	long int f = 1; //Сам факториал
+	cout << "Введите число, факториал которого надо найти" << endl;
+	cin >> n;
+	if (n == 0)
+	{
+		f = 1;
+	}
+	else
+	{
+		while (n > 0)
+		{
+			f = f * n;
+			n = n - 1;
+		}
+	}
+	cout << "Ваш факториал: " << f << endl;
+	return 0;
+}
