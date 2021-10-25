565d549f30ee2c25529d5c6c3e06f1fcc71f8fcb
Ilya Bolotov <92567941+IlyaBolotov@users.noreply.github.com>
2021-10-25 23:30:03 +0200

    Add files via upload

---
+++ ExersiseFromInternet.2.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+using namespace std;
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	int a;
+	int b;
+	int k;
+	int n;
+	int h;
+	cout << "Введите количество ступенек, на которые может прыгать заяц, и количество ступенек на лесенке" << endl;
+	cin >> a >> b;
+	while (n <= a)
+	{
+		if (h<n) 
+	}
+	cout << "Количество вариаций запрыгивания на лесенку: " << k << endl;
+	return 0;
+}
